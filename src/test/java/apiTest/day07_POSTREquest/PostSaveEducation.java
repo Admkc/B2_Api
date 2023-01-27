6dc28c188d6a3299db3ca284d03d36423e5d1954
Admkc <kocadem997103@gmail.com>
2023-01-28 01:23:37 +0300

    last1asuç

--- src/test/java/apiTest/day07_POSTREquest/PostSaveEducation.java
+++ src/test/java/apiTest/day07_POSTREquest/PostSaveEducation.java
@@ -4,6 +4,10 @@ import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import static org.hamcrest.Matchers.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.given;
@@ -37,8 +41,8 @@ public class PostSaveEducation {
         String token=response.path("token");
 
         String education="{\n" +
-                "  \"school\": \"namik kemal\",\n" +
-                "  \"degree\": \"takdir\",\n" +
+                "  \"school\": \"namik kemalOrt.\",\n" +
+                "  \"degree\": \"takdir10\",\n" +
                 "  \"study\": \"sayisal\",\n" +
                 "  \"fromdate\": \"1991-07-03\",\n" +
                 "  \"todate\": \"1995-08-02\",\n" +
@@ -46,10 +50,106 @@ public class PostSaveEducation {
                 "  \"description\": \"good work\"\n" +
                 "}";
 
+        response=given().accept(ContentType.JSON)
+                .and()
+                .contentType(ContentType.JSON)
+                .and()
+                .body(education)
+                .and()
+                .queryParam("token",token)
+                .when()
+                .post("/education/add");
+
+        assertEquals(response.statusCode(),200);
+        response.prettyPrint();
+
+    }
+    @Test
+    public void postNewUserAndVerify(){
+
+        String name="efe08";
+        String email="efe08@gmail.com";
+        String password="1245698";
+        String about="about me";
+        String terms="12";
+
+        Map<String ,Object> requestMap=new HashMap<>();
+
+        requestMap.put("name",name);
+        requestMap.put("email",email);
+        requestMap.put("password",password);
+        requestMap.put("about",about);
+        requestMap.put("terms",terms);
+
+        Response response=given().accept(ContentType.JSON)
+                .and()
+                .contentType(ContentType.JSON)
+                .body(requestMap)//serialization
+                .when()
+                .post("/allusers/register");
+
+
+        assertEquals(response.statusCode(),200);
+
+
+        String token=response.path("token");
 
 
 
+        Map<String ,Object> educationBody=new HashMap<>();
+        educationBody.put("school","Kraftecht");
+        educationBody.put("degree","Bootcamp");
+        educationBody.put("study","SDET");
+        educationBody.put("fromdate","2000-01-22");
+        educationBody.put("todate","2020-11-11");
+        educationBody.put("current","false");
+        educationBody.put("description","description11");
 
+        response=given().accept(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(educationBody)
+                .queryParam("token",token)
+                .when()
+                .post("education/add");
+
+        response.prettyPrint();
+
+        assertEquals(response.statusCode(),200);
+
+        //verify body
+
+        int id=response.path("id");
+
+        response=given().accept(ContentType.JSON)
+                .queryParam("token",token)
+                .pathParam("id",id)
+                .when()
+                .get("/education/getbyid/{id}");
+
+        response.prettyPrint();
+        assertEquals(response.statusCode(),200);
+
+        //verify with path
+
+        System.out.println("id = " + id);
+
+
+        assertEquals(response.path("school"),"Kraftech");
+
+        //verify using hamcrest matcher
+
+        given().accept(ContentType.JSON)
+                .and()
+                .queryParam("token",token)
+                .pathParam("id",id)
+                .when()
+                .get("/education/getbyid/{id}")
+
+                .then()
+                .assertThat()
+                .body("school",equalTo("Kraftech"),
+
+                        "study",equalTo("SDET")).log().all();
 
 
 
